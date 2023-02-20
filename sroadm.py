47434f955432e0685de44b03432be6fb4bd9b215
Ojas Tewari <60806481+mutedspeaker@users.noreply.github.com>
2023-02-20 01:19:38 +0000

    Update sroadm.py

--- sroadm.py
+++ sroadm.py
@@ -1,6 +1,16 @@
-from mnoptical.dataplane import Terminal, ROADM, OpticalLink, OpticalNet as Mininet, km, m, dB, dBm
+from mnoptical.dataplane import (Terminal, ROADM, OpticalLink,
+                       OpticalNet as Mininet, km, m, dB, dBm)
+from mnoptical.rest import RestServer
+from mnoptical.ofcdemo.demolib import OpticalCLI as CLI
+
 from mininet.node import OVSBridge, Host
 from mininet.topo import Topo
+from mininet.log import setLogLevel, warning, info
+from mininet.clean import cleanup
+
+from os.path import dirname, realpath, join
+from subprocess import run
+from sys import argv
 
 class SingleROADMTopo(Topo):
     def build(self):
@@ -19,6 +29,51 @@ class SingleROADMTopo(Topo):
         self.addLink(r1, t2, cls=OpticalLink, port1=2, port2=2, boost1=boost, spans=spans)
         self.addLink(r1, t3, cls=OpticalLink, port1=3, port2=2, spans=[1.0*m])
 
+        
+def plotNet(net, outfile="singleroadm.png", directed=False, layout='circo', colorMap=None, linksPerPair=5):
+    try:
+        import pygraphviz as pgv
+    except:
+        return
+    color = {ROADM: 'red', Terminal: 'blue', OVSBridge: 'orange', Host: 'black'}
+    if colorMap:
+        color.update(colorMap)
+    colors = {node: color.get(type(node), 'black') for node in net.values()}
+    nfont = {'fontname': 'helvetica bold', 'penwidth': 3}
+    g = pgv.AGraph(strict=False, directed=directed, layout=layout)
+    roadms = [node for node in net.switches if isinstance(node, ROADM)]
+    terms = [node for node in net.switches if isinstance(node, Terminal)]
+    other = [node for node in net.switches if node not in set(roadms+terms)]
+    for node in roadms + terms + other:
+        g.add_node(node.name, color=colors[node], **nfont)
+    for node in net.hosts:
+        g.add_node(node.name, color=colors[node], **nfont, shape='box')
+    linkcount = {}
+    for link in net.links:
+        intf1, intf2 = link.intf1, link.intf2
+        node1, node2 = intf1.node, intf2.node
+        port1, port2 = node1.ports[intf1], node2.ports[intf2]
+        linkcount[node1,node2] = linkcount.get((node1, node2),0) + 1
+        if linksPerPair is not None and linkcount[node1,node2] > linksPerPair:
+            continue
+        g.add_edge(node1.name, node2.name, headlabel=f' {node2}:{port2} ', taillabel=f' {node1}:{port1} ', labelfontsize=10, labelfontname='helvetica bold', penwidth=2)
+    g.layout()
+    g.draw(outfile)
+def test(net):
+    # Configure network and check connectivity
+    info('*** Configuring network and checking connectivity')
+    hosts = net.get('h1', 'h2')
+    testdir = dirname(realpath(argv[0]))
+    config_script = join(testdir, 'config-singleroadm.sh')
+    run(config_script)
+    assert net.ping(hosts, timeout=.5) == 0
+    
+    # Remove ROADM rule and check connectivity
+    info('*** Removing ROADM rule and checking connectivity')
+    remove_script = join(testdir, 'remove-singleroadm.sh')
+    run(remove_script)
+    assert net.ping(hosts, timeout=.5) == 100
+
 def main():
     topo = SingleROADMTopo()
     net = Mininet(topo=topo, switch=OVSBridge)
@@ -27,4 +82,16 @@ def main():
     net.stop()
 
 if __name__ == '__main__':
-    main()
+
+    cleanup()
+    setLogLevel('info')
+
+    topo = SingleROADMTopo()
+    net = Mininet(topo=topo, switch=OVSBridge, controller=None)
+    restServer = RestServer(net)
+    net.start()
+    restServer.start()
+    plotNet(net)
+    test(net) if 'test' in argv else CLI(net)
+    restServer.stop()
+    net.stop()
