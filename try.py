912454f7e6fc1ca1a6c2c38e8aaac718aeb15d08
Ojas Tewari <60806481+mutedspeaker@users.noreply.github.com>
2023-03-13 01:57:59 +0000

    Update try.py

--- try.py
+++ try.py
@@ -21,44 +21,37 @@ from os.path import dirname, realpath, join
 from subprocess import run
 from sys import argv
 
-
 class SingleROADMTopo(Topo):
-    """
-    h1 - s1 - t1 - r1 -- r2 -- r3 -- t11,t12,t13,t14,t15 - s11,s12,s13,s14,s15  - h11,h12,h13,h14,h15
-                ||    ||
-   t2,t3,t4,t5    t6,t7,t8,t9,t10
-                ||    ||
- h2,h3,h4,h5    h6,h7,h8,h9,h10
-    """
     def build(self):
-        # Create hosts and switches
-        hosts = [self.addHost(f"h{i}") for i in range(1, 16)]
-        switches = [self.addSwitch(f"s{i}") for i in range(1, 16)]
-        # Create terminals and roadms
-        terminals = [self.addSwitch(f"t{i}", cls=Terminal, transceivers=[('tx1', 0*dBm, 'C')], monitor_mode='in') for i in range(1, 6)]
-        roadms = [self.addSwitch(f"r{i}", cls=ROADM) for i in range(1, 4)]
-        # Create links between hosts and switches
-        for i in range(1, 6):
-            self.addLink(hosts[i-1], switches[0])
-        for i in range(6, 16):
-            self.addLink(hosts[i-1], switches[i-5])
-        # Create links between switches and terminals
-        for i in range(1, 6):
-            self.addLink(switches[i-1], terminals[i-1], port2=1)
-        # Create links between terminals and roadms
-        boost = ('boost', {'target_gain': 3.0*dB})
-        amp1 = ('amp1', {'target_gain': 25*.22*dB})
-        amp2 = ('amp2', {'target_gain': 25*.22*dB})
-        spans = [25*km, amp1, 25*km, amp2]
-        for i in range(1, 6):
-            self.addLink(terminals[i-1], roadms[0], cls=OpticalLink, port1=i+1, port2=i+1, boost1=boost, spans=spans)
-            self.addLink(terminals[i-1], roadms[1], cls=OpticalLink, port1=i+6, port2=i+6, boost1=boost, spans=spans)
-        # Create links between roadms
-        self.addLink(roadms[0], roadms[1], cls=OpticalLink, port1=3, port2=3, boost1=boost, spans=spans)
-        self.addLink(roadms[1], roadms[2], cls=OpticalLink, port1=4, port2=4, boost1=boost, spans=spans)
-        # Create links between switches and roadms
-        for i in range(1, 6):
-            self.addLink(switches[i-1], roadms[1], cls=OpticalLink, port1=i+10, port2=i+10, boost1=boost, spans=spans)
+        h1, h2, h3, h4 = [self.addHost(f'h{i}') for i in range(1, 5)]
+        s1, s2, s3, s4 = [self.addSwitch(f's{i}') for i in range(1, 5)]
+        t1, t2, t3, t4, t5, t6, t7, t8, t9, t10 = [
+            self.addSwitch(f't{i}', cls=Terminal, transceivers=[('tx1', 0*dBm, 'C')],
+            monitor_mode='in') for i in range(1, 11)]
+        t11, t12, t13, t14, t15 = [self.addSwitch(f't{i}', cls=Terminal, monitor_mode='in') for i in range(11, 16)]
+        r1, r2, r3 = [self.addSwitch(f'r{i}', cls=ROADM) for i in range(1, 4)]
+
+        # Add links
+        for i, (src, dst) in enumerate([(h1, s1), (h2, s2), (h3, s3), (h4, s4)]):
+            self.addLink(src, dst)
+        for i, (src, dst) in enumerate([(s1, t1), (s2, t2), (s3, t3)]):
+            self.addLink(src, dst, port2=1)
+        for i, (src, dst) in enumerate([(s1, r1), (r1, r2), (r2, r3), (r3, s4)]):
+            boost = ('boost', {'target_gain': 3.0*dB})
+            amp1 = ('amp1', {'target_gain': 25*.22*dB})
+            amp2 = ('amp2', {'target_gain': 25*.22*dB})
+            spans = [25*km, amp1, 25*km, amp2]
+            self.addLink(src, dst, cls=OpticalLink, boost1=boost, spans=spans)
+        for i, (src, dst) in enumerate([(r1, t11), (r2, t12), (r2, t13), (r3, t14), (r3, t15)]):
+            self.addLink(src, dst, cls=OpticalLink)
+        for i, (src, dst) in enumerate([(s4, t6), (s4, t7), (s4, t8), (s4, t9), (s4, t10)]):
+            self.addLink(src, dst, port1=i+1)
+
+
+
+
+
+
 # Debugging: Plot network graph
 def plotNet(net, outfile="gConfigThreeRoadms.png", directed=False, layout='circo',
             colorMap=None, linksPerPair=5):
