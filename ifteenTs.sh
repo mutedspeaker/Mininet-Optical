ac54943b9cf0766e925cf1ae6bc39f7c563b8af6
Ojas Tewari <60806481+mutedspeaker@users.noreply.github.com>
2023-03-14 09:06:50 +0000

    Update ifteenTs.sh

--- ifteenTs.sh
+++ ifteenTs.sh
@@ -3,15 +3,26 @@
 set -e  # exit script on error
 
 # URL for REST server
-url="localhost:8080"; t1=$url; t2=$url; t3=$url;r1=$url; r2=$url; r3=$url
+url="localhost:8080"; t1=$url; t2=$url; t3=$url; t4=$url; t5=$url; t6=$url; t7=$url; t8=$url; t9=$url; t10=$url; t11=$url; t12=$url; t13=$url; t14=$url; t15=$url; r1=$url; r2=$url; r3=$url
 
 curl="curl -s"
 
 echo "* Configuring terminals in threeRoadms.py network"
-
 $curl "$t1/connect?node=t1&ethPort=3&wdmPort=2&channel=41"
 $curl "$t2/connect?node=t2&ethPort=7&wdmPort=7&channel=45"
 $curl "$t3/connect?node=t3&ethPort=5&wdmPort=5&channel=49"
+$curl "$t4/connect?node=t4&ethPort=8&wdmPort=8&channel=50"
+$curl "$t5/connect?node=t5&ethPort=2&wdmPort=3&channel=42"
+$curl "$t6/connect?node=t6&ethPort=1&wdmPort=10&channel=51"
+$curl "$t7/connect?node=t7&ethPort=4&wdmPort=11&channel=52"
+$curl "$t8/connect?node=t8&ethPort=6&wdmPort=12&channel=53"
+$curl "$t9/connect?node=t9&ethPort=7&wdmPort=13&channel=54"
+$curl "$t10/connect?node=t10&ethPort=8&wdmPort=15&channel=55"
+$curl "$t11/connect?node=t11&ethPort=4&wdmPort=16&channel=56"
+$curl "$t12/connect?node=t12&ethPort=5&wdmPort=17&channel=57"
+$curl "$t13/connect?node=t13&ethPort=6&wdmPort=18&channel=58"
+$curl "$t14/connect?node=t14&ethPort=1&wdmPort=19&channel=59"
+$curl "$t15/connect?node=t15&ethPort=2&wdmPort=20&channel=60"
 
 echo "* Resetting ROADM"
 $curl "$r1/reset?node=r1"
@@ -43,13 +54,13 @@ $curl "$t1/turn_on?node=t1"
 $curl "$t2/turn_on?node=t2"
 $curl "$t3/turn_on?node=t3"
 
-for tname in t1 t2 t3; do
+for tname in t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11 t12 t13 t14 t15; do
     url=${!tname}
     $curl "$url/monitor?monitor=$tname-monitor"
 done
 
 echo "* Monitoring signals at endpoints"
-for tname in t1 t2 t3; do
+for tname in t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11 t12 t13 t14 t15; do
     url=${!tname}
     echo "* $tname"
     $curl "$url/monitor?monitor=$tname-monitor"
