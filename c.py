96e490365f2d24501d2ce230a9e838130e40764e
Ojas Tewari <60806481+mutedspeaker@users.noreply.github.com>
2023-03-16 16:07:50 +0000

    Update c.py

--- c.py
+++ c.py
@@ -1,2 +1,6 @@
-import commands
-print (commands.getstatusoutput('echo "test" | wc'))
+import subprocess
+
+# Run a command and capture its output
+cmd = "ls -l"
+output = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+print(output)
