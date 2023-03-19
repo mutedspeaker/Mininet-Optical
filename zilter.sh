74cfc2c6fb0b149db9efc945895572b676d85a64
Ojas Tewari <60806481+mutedspeaker@users.noreply.github.com>
2023-03-19 21:55:58 +0000

    Update zilter.sh

--- zilter.sh
+++ zilter.sh
@@ -1,2 +1,10 @@
-awk '/^t[0-9]+ receiving/ {match($0, /<ch([1-9][0-9]?):([0-9]+.[0-9]+)THz>/, arr); printf "%s ch%s %s gOSNR: %s dB OSNR: %s dB\n", $1, arr[1], arr[2], $(NF-1), $NF}' final.txt > final2.txt
-
+while read -r input; do
+  if [[ "$input" =~ ^(t[0-9]+) receiving \<ch([0-9]+):([0-9]+\.[0-9]+)THz\> at port [0-9]+: Success! gOSNR: ([0-9]+\.[0-9]+) dB OSNR: ([0-9]+\.[0-9]+) dB$ ]]; then
+    t="${BASH_REMATCH[1]}"
+    ch="${BASH_REMATCH[2]}"
+    freq="${BASH_REMATCH[3]}"
+    gosnr="${BASH_REMATCH[4]}"
+    osnr="${BASH_REMATCH[5]}"
+    echo "t=$t, ch=$ch, freq=$freq, gOSNR=$gosnr, OSNR=$osnr"
+  fi
+done < output.txt
