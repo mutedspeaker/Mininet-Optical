63335462fcb9b817dd3f588639180a25eccdfc41
mutedspeaker <ojasvtewari@gmail.com>
2023-03-19 21:45:02 +0000

    zilter

---
+++ zilter.sh
@@ -0,0 +1,2 @@
+awk '/^t[0-9]+ receiving/ {match($0, /<ch([1-9][0-9]?):([0-9]+.[0-9]+)THz>/, arr); printf "%s ch%s %s gOSNR: %s dB OSNR: %s dB\n", $1, arr[1], arr[2], $(NF-1), $NF}' final.txt > final2.txt
+
