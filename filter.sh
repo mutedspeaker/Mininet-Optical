31f6a85930e25a66b966941c338479c6029e7b9f
Ojas Tewari <60806481+mutedspeaker@users.noreply.github.com>
2023-03-19 21:25:31 +0000

    Update filter.sh

--- filter.sh
+++ filter.sh
@@ -1,5 +1,4 @@
 awk '/^(*** t)[1-9][1-9]?( )/  {print substr($0,5)}'  output.txt > final.txt
 
-awk '/^t[0-9]+ receiving/ {match($0, /<ch([0-9]+):([0-9]+\.[0-9]+)THz>/, arr); printf "%s ch%s %s gOSNR: %s dB OSNR: %s dB\n", $1, arr[1], arr[2], $(NF-1), $NF}' final.txt > final2.txt
-
+awk '/^t[0-9]+ receiving/ {match($0, /<ch([1-9][0-9]?):([0-9]+.[0-9]+)THz>/, arr); printf "%s ch%s %s gOSNR: %s dB OSNR: %s dB\n", $1, arr[1], arr[2], $(NF-1), $NF}' output.txt > final2.txt
 
