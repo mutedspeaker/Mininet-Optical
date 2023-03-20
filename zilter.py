8fa276ce5c04fbb40bf2071318246e1d49232c69
Ojas Tewari <60806481+mutedspeaker@users.noreply.github.com>
2023-03-20 20:21:14 +0000

    Update zilter.py

--- zilter.py
+++ zilter.py
@@ -7,6 +7,7 @@ with open(input_file, "r") as f_in, open(output_file, "w", newline="") as f_out:
     writer = csv.writer(f_out)
     writer.writerow(["ch", "gOSNR", "OSNR"]) # write header row
     
+    prev_ch = None # to keep track of previous channel value
     for line in f_in:
         # extract fields from line
         fields = line.strip().split()
@@ -18,5 +19,19 @@ with open(input_file, "r") as f_in, open(output_file, "w", newline="") as f_out:
         gOSNR = Decimal(fields[8])
         OSNR = Decimal(fields[11])
         
+        # check if current ch is missing and add with gOSNR and OSNR as 0
+        if prev_ch is not None and ch != prev_ch + 1:
+            for i in range(prev_ch + 1, ch):
+                writer.writerow([i, 0, 0])
+                
         # write fields to CSV
         writer.writerow([ch, gOSNR, OSNR])
+        prev_ch = ch # update previous channel value
+       
+        # add missing channels after the last one up to 90
+        if ch == 90:
+            break
+        if prev_ch is None:
+            for i in range(1, ch):
+                writer.writerow([i, 0, 0])
+            prev_ch = ch
